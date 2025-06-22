94466dcc04223e7f334401b806ad4a06e4bf8d22
kyle-j13 <kylejohnston458@gmail.com>
2025-06-22 10:51:25 -0400

    Import VLC source code

---
+++ modules/gui/qt/maininterface/videosurface.cpp
@@ -0,0 +1,370 @@
+/*****************************************************************************
+ * Copyright (C) 2019 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+#include "videosurface.hpp"
+
+#include <QSGRectangleNode>
+#include <QThreadPool>
+#include <vlc_window.h>
+
+#include <QQuickRenderControl>
+#ifdef QT_DECLARATIVE_PRIVATE
+#  include <QtGui/qpa/qplatformwindow.h>
+#endif
+
+#include "maininterface/mainctx.hpp"
+
+VideoSurfaceProvider::VideoSurfaceProvider(bool threadedSurfaceUpdates, QObject* parent)
+    : QObject(parent)
+    , m_threadedSurfaceUpdates(threadedSurfaceUpdates)
+{
+}
+
+bool VideoSurfaceProvider::isEnabled()
+{
+    return m_voutWindow != nullptr;
+}
+
+bool VideoSurfaceProvider::hasVideoEmbed() const
+{
+    return m_videoEmbed;
+}
+
+void VideoSurfaceProvider::enable(vlc_window_t* voutWindow)
+{
+    assert(voutWindow);
+    m_voutWindow = voutWindow;
+    emit videoEnabledChanged(true);
+}
+
+void VideoSurfaceProvider::disable()
+{
+    setVideoEmbed(false);
+    m_voutWindow = nullptr;
+    emit videoEnabledChanged(false);
+}
+
+void VideoSurfaceProvider::setVideoEmbed(bool embed)
+{
+    m_videoEmbed = embed;
+    emit hasVideoEmbedChanged(embed);
+}
+
+void VideoSurfaceProvider::onWindowClosed()
+{
+    if (m_voutWindow)
+        vlc_window_ReportClose(m_voutWindow);
+}
+
+void VideoSurfaceProvider::onMousePressed(int vlcButton)
+{
+    if (m_voutWindow)
+        vlc_window_ReportMousePressed(m_voutWindow, vlcButton);
+}
+
+void VideoSurfaceProvider::onMouseReleased(int vlcButton)
+{
+    if (m_voutWindow)
+        vlc_window_ReportMouseReleased(m_voutWindow, vlcButton);
+}
+
+void VideoSurfaceProvider::onMouseDoubleClick(int vlcButton)
+{
+    if (m_voutWindow)
+        vlc_window_ReportMouseDoubleClick(m_voutWindow, vlcButton);
+}
+
+void VideoSurfaceProvider::onMouseMoved(float x, float y)
+{
+    if (m_voutWindow)
+        vlc_window_ReportMouseMoved(m_voutWindow, x, y);
+}
+
+void VideoSurfaceProvider::onMouseWheeled(int vlcButton)
+{
+    if (m_voutWindow)
+        vlc_window_ReportKeyPress(m_voutWindow, vlcButton);
+}
+
+void VideoSurfaceProvider::onKeyPressed(int key, Qt::KeyboardModifiers modifiers)
+{
+    QKeyEvent event(QEvent::KeyPress, key, modifiers);
+    int vlckey = qtEventToVLCKey(&event);
+    if (m_voutWindow)
+        vlc_window_ReportKeyPress(m_voutWindow, vlckey);
+}
+
+void VideoSurfaceProvider::onSurfaceSizeChanged(QSizeF size)
+{
+    emit surfaceSizeChanged(size);
+    if (m_voutWindow)
+        vlc_window_ReportSize(m_voutWindow, std::ceil(size.width()), std::ceil(size.height()));
+}
+
+
+VideoSurface::VideoSurface(QQuickItem* parent)
+    : ViewBlockingRectangle(parent)
+{
+    setAcceptHoverEvents(true);
+    setAcceptedMouseButtons(Qt::AllButtons);
+    setFlag(ItemAcceptsInputMethod, true);
+}
+
+int VideoSurface::qtMouseButton2VLC( Qt::MouseButton qtButton )
+{
+    switch( qtButton )
+    {
+        case Qt::LeftButton:
+            return 0;
+        case Qt::RightButton:
+            return 2;
+        case Qt::MiddleButton:
+            return 1;
+        default:
+            return -1;
+    }
+}
+
+void VideoSurface::mousePressEvent(QMouseEvent* event)
+{
+    int vlc_button = qtMouseButton2VLC( event->button() );
+    if( vlc_button >= 0 )
+    {
+        emit mousePressed(vlc_button);
+        event->accept();
+    }
+    else
+        event->ignore();
+}
+
+void VideoSurface::mouseReleaseEvent(QMouseEvent* event)
+{
+    int vlc_button = qtMouseButton2VLC( event->button() );
+    if( vlc_button >= 0 )
+    {
+        emit mouseReleased(vlc_button);
+        event->accept();
+    }
+    else
+        event->ignore();
+}
+
+void VideoSurface::mouseMoveEvent(QMouseEvent* event)
+{
+    QPointF current_pos = event->position();
+    QQuickWindow* window = this->window();
+    if (!window)
+        return;
+    qreal dpr = window->effectiveDevicePixelRatio();
+    emit mouseMoved(current_pos.x() * dpr, current_pos.y() * dpr);
+    event->accept();
+}
+
+void VideoSurface::hoverMoveEvent(QHoverEvent* event)
+{
+    QPointF current_pos = event->position();
+    if (current_pos != m_oldHoverPos)
+    {
+        QQuickWindow* window = this->window();
+        if (!window)
+            return;
+        qreal dpr = window->effectiveDevicePixelRatio();
+        emit mouseMoved(current_pos.x() * dpr, current_pos.y()  * dpr);
+        m_oldHoverPos = current_pos;
+    }
+    event->accept();
+}
+
+void VideoSurface::mouseDoubleClickEvent(QMouseEvent* event)
+{
+    int vlc_button = qtMouseButton2VLC( event->button() );
+    if( vlc_button >= 0 )
+    {
+        emit mouseDblClicked(vlc_button);
+        event->accept();
+    }
+    else
+        event->ignore();
+}
+
+void VideoSurface::keyPressEvent(QKeyEvent* event)
+{
+    emit keyPressed(event->key(), event->modifiers());
+    event->ignore();
+}
+
+#if QT_CONFIG(wheelevent)
+void VideoSurface::wheelEvent(QWheelEvent *event)
+{
+    m_wheelEventConverter.wheelEvent(event);
+    event->accept();
+}
+#endif
+
+Qt::CursorShape VideoSurface::getCursorShape() const
+{
+    return cursor().shape();
+}
+
+void VideoSurface::setCursorShape(Qt::CursorShape shape)
+{
+    setCursor(shape);
+}
+
+void VideoSurface::synchronize()
+{
+    // This may be called from the rendering thread, not necessarily
+    // during synchronization (GUI thread is not blocked). Try to
+    // be very careful.
+
+    QSizeF size;
+    QPointF position;
+
+    static const bool isObjectThread = QThread::currentThread() == thread();
+    if (isObjectThread)
+    {
+        assert(QThread::currentThread() == thread());
+        // Item's thread (GUI thread):
+        size = this->size();
+        position = this->mapToScene(QPointF(0,0));
+    }
+    else
+    {
+        assert(QThread::currentThread() != thread());
+        // Render thread:
+        size = renderSize();
+        position = renderPosition();
+    }
+
+    if (m_oldRenderSize != size || m_dprDirty)
+    {
+        if (!size.isEmpty())
+        {
+            emit surfaceSizeChanged(size * m_dpr);
+            m_oldRenderSize = size;
+        }
+    }
+
+    if (m_oldRenderPosition != position || m_dprDirty)
+    {
+        if (position.x() >= 0.0 && position.y() >= 0.0)
+        {
+            emit surfacePositionChanged(position * m_dpr); // render position is relative to scene/viewport
+            m_oldRenderPosition = position;
+        }
+    }
+
+    if (m_dprDirty)
+    {
+        emit surfaceScaleChanged(m_dpr);
+        m_dprDirty = false;
+    }
+}
+
+void VideoSurface::itemChange(ItemChange change, const ItemChangeData &value)
+{
+    if (change == ItemDevicePixelRatioHasChanged || change == ItemSceneChange)
+    {
+        m_dprChanged = true;
+        // Request update, so that `updatePaintNode()` gets called which updates the DPR for `::synchronize()`:
+        update();
+    }
+
+    QQuickItem::itemChange(change, value);
+}
+
+void VideoSurface::setVideoSurfaceProvider(VideoSurfaceProvider *newVideoSurfaceProvider)
+{
+    if (m_provider == newVideoSurfaceProvider)
+        return;
+
+    if (m_provider)
+    {
+        disconnect(this, nullptr, m_provider, nullptr);
+        disconnect(&m_wheelEventConverter, nullptr, m_provider, nullptr);
+        disconnect(m_provider, nullptr, this, nullptr);
+    }
+
+    m_provider = newVideoSurfaceProvider;
+
+    if (m_provider)
+    {
+        connect(this, &VideoSurface::mouseMoved, m_provider, &VideoSurfaceProvider::onMouseMoved);
+        connect(this, &VideoSurface::mousePressed, m_provider, &VideoSurfaceProvider::onMousePressed);
+        connect(this, &VideoSurface::mouseDblClicked, m_provider, &VideoSurfaceProvider::onMouseDoubleClick);
+        connect(this, &VideoSurface::mouseReleased, m_provider, &VideoSurfaceProvider::onMouseReleased);
+        connect(this, &VideoSurface::keyPressed, m_provider, &VideoSurfaceProvider::onKeyPressed);
+        connect(this, &VideoSurface::surfaceSizeChanged, m_provider, &VideoSurfaceProvider::onSurfaceSizeChanged, Qt::DirectConnection);
+        connect(this, &VideoSurface::surfacePositionChanged, m_provider, &VideoSurfaceProvider::surfacePositionChanged, Qt::DirectConnection);
+        connect(this, &VideoSurface::surfaceScaleChanged, m_provider, &VideoSurfaceProvider::surfaceScaleChanged, Qt::DirectConnection);
+
+        connect(&m_wheelEventConverter, &WheelToVLCConverter::vlcWheelKey, m_provider, &VideoSurfaceProvider::onMouseWheeled);
+
+        setFlag(ItemHasContents, true);
+    }
+    else
+    {
+        setFlag(ItemHasContents, false);
+    }
+
+    emit videoSurfaceProviderChanged();
+}
+
+QSGNode *VideoSurface::updatePaintNode(QSGNode *node, UpdatePaintNodeData *data)
+{
+    // This is called from the render thread, but during synchronization.
+    // So the GUI thread is blocked here. This makes it safer to access the window
+    // to get the effective DPR, rather than doing it outside the synchronization
+    // stage.
+
+    const auto w = window();
+    assert (w);
+
+    if (Q_UNLIKELY(!m_provider))
+        return node;
+
+    if (w != m_oldWindow)
+    {
+        if (m_oldWindow)
+            disconnect(m_synchConnection);
+
+        m_oldWindow = w;
+
+        if (w)
+        {
+            // This is constant:
+            if (m_provider->supportsThreadedSurfaceUpdates())
+            {
+                // Synchronize just before swapping the frame for better synchronization:
+                m_synchConnection = connect(w, &QQuickWindow::afterRendering, this, &VideoSurface::synchronize, Qt::DirectConnection);
+            }
+            else
+            {
+                m_synchConnection = connect(w, &QQuickWindow::afterAnimating, this, &VideoSurface::synchronize);
+            }
+        }
+    }
+
+    if (m_dprChanged)
+    {
+        m_dpr = w->effectiveDevicePixelRatio();
+        m_dprDirty = true;
+        m_dprChanged = false;
+    }
+
+    return ViewBlockingRectangle::updatePaintNode(node, data);
+}
---
+++ modules/gui/qt/medialibrary/qml/ArtistTopBanner.qml
@@ -0,0 +1,226 @@
+/*****************************************************************************
+ * Copyright (C) 2019 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * ( at your option ) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+import QtQuick
+import QtQuick.Controls
+import QtQuick.Layouts
+import QtQuick.Window
+import QtQml.Models
+
+import VLC.MainInterface
+import VLC.MediaLibrary
+
+import VLC.Widgets as Widgets
+import VLC.Style
+import VLC.Util
+
+FocusScope {
+    id: root
+
+    property int rightPadding: 0
+
+    property var artist: ({})
+
+    implicitHeight: VLCStyle.artistBanner_height
+
+    Accessible.role: Accessible.Pane
+    Accessible.name: qsTr("Artist banner")
+
+    function setCurrentItemFocus(reason) {
+        playActionBtn.forceActiveFocus(reason);
+    }
+
+    readonly property ColorContext colorContext: ColorContext {
+        id: theme
+        ////force the dark theme in the header
+        palette: VLCStyle.darkPalette
+        colorSet: ColorContext.View
+    }
+
+    Image {
+        id: background
+
+        // NOTE: We want the banner to ignore safe margins.
+        anchors.fill: parent
+
+        asynchronous: true
+        source: artist.cover || VLCStyle.noArtArtist
+        sourceSize: artist.cover ? Qt.size(Helpers.alignUp(Screen.desktopAvailableWidth, 32), 0)
+                                 : undefined
+        mipmap: !!artist.cover
+        // Fill mode is stretch when blur effect is used, otherwise an implicit layer is created.
+        // Having the fill mode stretch does not have a side effect here, because source size
+        // is still calculated as to preserve the aspect ratio as height is left empty (0) and the
+        // image is not shown stretched because it is invisible when blur effect is used:
+        // "If only one dimension of the size is set to greater than 0, the other dimension is
+        //  set in proportion to preserve the source image's aspect ratio. (The fillMode is
+        //  independent of this.)". Unfortunately with old Qt versions we can not do this
+        // because it does not seem to create a layer when fill mode (tiling is wanted)
+        // is changed at a later point.
+        fillMode: artist.cover ? ((visible || (MainCtx.qtVersion() < MainCtx.qtVersionCheck(6, 5, 0))) ? Image.PreserveAspectCrop : Image.Stretch)
+                                : Image.Tile
+
+        visible: !blurEffect.visible
+        cache: (source === VLCStyle.noArtArtist)
+
+        // In fact, since layering is not used blur effect
+        // would not care about the opacity here. Still,
+        // to show intention we can have a simple binding:
+        opacity: blurEffect.visible ? 1.0 : 0.5
+    }
+
+    Item {
+        anchors.fill: background
+
+        // The texture is big, and the blur item should only draw the portion of it.
+        // If the blur effect creates an implicit layer, it properly adjusts the
+        // area that it needs to cover. However, as we don't want an additional
+        // layer that keeps getting updated every time the size changes, we feed
+        // the whole static texture. For that reason, we need clipping because
+        // the blur effect is applied to the whole texture and shown as whole:
+        clip: !blurEffect.sourceNeedsLayering
+
+        visible: (GraphicsInfo.shaderType === GraphicsInfo.RhiShader)
+
+        // This blur effect does not create an implicit layer that is updated
+        // each time the size changes. The source texture is static, so the blur
+        // is applied only once and we adjust the viewport through the parent item
+        // with clipping.
+        Widgets.BlurEffect {
+            id: blurEffect
+
+            anchors.verticalCenter: parent.verticalCenter
+            anchors.left: parent.left
+            anchors.right: parent.right
+
+            // If source image is tiled, layering is necessary:
+            readonly property bool sourceNeedsLayering: (background.fillMode !== Image.Stretch) ||
+                                                        (MainCtx.qtVersion() < MainCtx.qtVersionCheck(6, 5, 0))
+
+            readonly property real aspectRatio: (background.implicitHeight / background.implicitWidth)
+
+            height: sourceNeedsLayering ? background.height : (aspectRatio * width)
+
+            source: background
+
+            radius: VLCStyle.dp(4, VLCStyle.scale)
+        }
+    }
+
+    Rectangle {
+        anchors.fill: background
+        gradient: Gradient {
+            GradientStop { position: 0.0; color: Qt.rgba(0, 0, 0, .5) }
+            GradientStop { position: 1.0; color: Qt.rgba(0, 0, 0, .7) }
+        }
+    }
+
+    RowLayout {
+        id: col
+
+        anchors.fill: parent
+        anchors.topMargin: VLCStyle.margin_xxlarge
+        anchors.bottomMargin: VLCStyle.margin_xxlarge
+        anchors.leftMargin: VLCStyle.margin_xlarge
+        anchors.rightMargin: root.rightPadding
+
+        spacing: VLCStyle.margin_normal
+
+        Item {
+            implicitHeight: VLCStyle.cover_normal
+            implicitWidth: VLCStyle.cover_normal
+
+            Widgets.ImageExt {
+                id: roundImage
+                source: artist.cover || VLCStyle.noArtArtist
+                sourceSize: Qt.size(width * eDPR, height * eDPR)
+                anchors.fill: parent
+                radius: VLCStyle.cover_normal
+                borderColor: theme.border
+                borderWidth: VLCStyle.dp(1, VLCStyle.scale)
+                fillMode: Image.PreserveAspectCrop
+                readonly property real eDPR: MainCtx.effectiveDevicePixelRatio(Window.window)
+            }
+        }
+
+        ColumnLayout {
+            Layout.fillWidth: true
+
+            // NOTE: The layout can be resized to zero to hide the text entirely.
+            Layout.minimumWidth: 0
+
+            Layout.rightMargin: VLCStyle.margin_small
+
+            spacing: 0
+
+            Widgets.SubtitleLabel {
+                Layout.fillWidth: true
+
+                text: artist.name || qsTr("No artist")
+                color: theme.fg.primary
+
+                Layout.maximumWidth: parent.width
+            }
+
+            Widgets.MenuCaption {
+                Layout.fillWidth: true
+
+                Layout.topMargin: VLCStyle.margin_xxxsmall
+
+                text: qsTr("%1 Songs").arg(artist.nb_tracks)
+                color: theme.fg.secondary
+            }
+
+            Widgets.NavigableRow {
+                id: actionButtons
+
+                focus: true
+                Navigation.parentItem: root
+                spacing: VLCStyle.margin_large
+
+                Layout.fillWidth: true
+                Layout.topMargin: VLCStyle.margin_large
+
+                model: ObjectModel {
+                    Widgets.ActionButtonPrimary {
+                        id: playActionBtn
+                        iconTxt: VLCIcons.play
+                        text: qsTr("Play all")
+                        focus: true
+
+                        //we probably want to keep this button like the other action buttons
+                        colorContext.palette: VLCStyle.palette
+
+                        onClicked: MediaLib.addAndPlay( artist.id )
+                    }
+
+                    Widgets.ActionButtonOverlay {
+                        id: enqueueActionBtn
+                        iconTxt: VLCIcons.enqueue
+                        text: qsTr("Enqueue all")
+                        onClicked: MediaLib.addToPlaylist( artist.id )
+                    }
+                }
+            }
+
+            Item {
+                Layout.fillWidth: true
+            }
+        }
+    }
+
+}
